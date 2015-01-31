4361cda5600fd9053304485e6e160fecd40d9627
yosu <woodstock830@gmail.com>
2015-01-31 16:08:46 +0000

    Move nvm installer

--- scrips/install_nvm.sh
+++ scrips/install_nvm.sh
@@ -6,26 +6,23 @@
 
 NVM_REPO=https://github.com/creationix/nvm.git 
 NVM_ROOT=$HOME/.nvm
-RC_FILE=$HOME/.zshrc.local
+RC_FILE=$HOME/.zsh-sources/nvm
 
 die () {
     echo $1 >&2
     exit 1
 }
 
-write_profile () {
-    if [ $(grep "$1" $RC_FILE | wc -l) -le 0 ] ; then
-        echo "$1" >> $RC_FILE
-    fi
-}
-
 install_nvm () {
     git --version || die "git is not installed."
 
-    if [[ ! -d $NVM_ROOT ]]; then
-        git clone $NVM_REPO $NVM_ROOT || die "git clone failure $NVM_REPO"
-	write_profile 'source ~/.nvm/nvm.sh'
+    if [[ -d $NVM_ROOT ]]; then
+        echo nvm is already installed
+        exit 0
     fi
+
+    git clone $NVM_REPO $NVM_ROOT || die "git clone failure $NVM_REPO"
+    echo 'source ~/.nvm/nvm.sh' > $RC_FILE
 }
 
 install_nvm
