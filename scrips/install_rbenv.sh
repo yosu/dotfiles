c9de4cc48cee93f76b0015e56eadba91d552db41
yosu <woodstock830@gmail.com>
2015-01-31 15:59:57 +0000

    Integrate rbenv installation

--- scrips/install_rbenv.sh
+++ scrips/install_rbenv.sh
@@ -4,12 +4,10 @@
 #   rbenvsetup http://is.gd/rbenvsetup
 #   Tue May 28 11:03:00 JST 2013 v0.01 by ytnobody
 #
-# Modfied:
-# - PROF_FILE: bash_profile -> zshrc
 
 RBENV_REPO=git://github.com/sstephenson/rbenv.git
 RBENV_ROOT=$HOME/.rbenv
-PROF_FILE=$HOME/.zshrc.local
+PROF_FILE=$HOME/.zsh-sources/rbenv
 
 RUBYBUILDER_REPO=git://github.com/sstephenson/ruby-build.git
 RBENV_PLUGIN_DIR=$RBENV_ROOT/plugins
@@ -19,19 +17,15 @@ die () {
     exit 1
 }
 
-write_profile () {
-    if [ $(grep "$1" $PROF_FILE | wc -l) -le 0 ] ; then
-        echo "$1" >> $PROF_FILE
-    fi
-}
-
 git --version || die "git is not installed."
 
 ### setup rbenv
 git clone $RBENV_REPO $RBENV_ROOT || die "git clone failure : $RBENV_REPO"
-write_profile 'export RBENV_ROOT=$HOME/.rbenv'
-write_profile 'export PATH=$PATH:$RBENV_ROOT/bin'
-write_profile 'eval "$(rbenv init -)"'
+cat << 'END' > $PROF_FILE
+export RBENV_ROOT=$HOME/.rbenv
+export PATH=$PATH:$RBENV_ROOT/bin
+eval "$(rbenv init -)"
+END
 
 ### setup ruby-builder
 mkdir $RBENV_PLUGIN_DIR
@@ -40,6 +34,3 @@ git clone $RUBYBUILDER_REPO $RBENV_PLUGIN_DIR/ruby-build || die "git clone failu
 ### setup plugins
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git $RBENV_PLUGIN_DIR/rbenv-gem-rehash || die "git clone failure : rbenv-gem-rehash"
 git clone https://github.com/ianheggie/rbenv-binstubs.git $RBENV_PLUGIN_DIR/rbenv-binstubs || die "git clone failure : rbenv-binstubs"
-
-### load profile
-exec $SHELL -l
