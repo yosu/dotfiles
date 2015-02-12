e096aa79e576045b85218ed413ba8bd858c8cd86
yosu <woodstock830@gmail.com>
2015-02-12 23:48:09 +0000

    Add golang installer

---
+++ scripts/install_go.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+OS=${OS:-linux}
+VERSION=${VERSION:-1.4.1}
+ARCH=${ARCH:-amd64}
+GO_SRC=go${VERSION}.${OS}-${ARCH}.tar.gz
+PROF_FILE=$HOME/.zsh-sources/go
+
+DEST_DIR=${DEST_DIR:-$HOME}
+GOROOT=$DEST_DIR/go
+
+if [ ! -d $GOROOT ];then
+    mkdir $GOROOT
+fi
+
+wget https://storage.googleapis.com/golang/${GO_SRC}
+tar -C $DEST_DIR -xzf $GO_SRC
+rm $GO_SRC
+
+cat << END > $PROF_FILE
+export GOROOT=$GOROOT
+export PATH=\$PATH:\$GOROOT/bin
+END
