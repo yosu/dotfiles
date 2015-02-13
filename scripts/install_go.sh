862d0161b47ae64dec69f71ecd76f5069ceef753
yosu <woodstock830@gmail.com>
2015-02-13 14:16:11 +0000

    Add golang plugins

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -14,12 +14,12 @@ if [ ! -d $GOROOT ];then
     mkdir $GOROOT
 fi
 
-#wget https://storage.googleapis.com/golang/${GO_SRC}
-#tar -C $DEST_DIR -xzf $GO_SRC
-#rm $GO_SRC
+wget https://storage.googleapis.com/golang/${GO_SRC}
+tar -C $DEST_DIR -xzf $GO_SRC
+rm $GO_SRC
 
 cat << END > $PROF_FILE
 export GOROOT=$GOROOT
 export GOPATH=\$HOME/.go
-export PATH=\$PATH:\$GOROOT/bin:\$GOPATH
+export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin
 END
