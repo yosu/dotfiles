0a3e6b4c1f70b3dc289784eeb61a6f8659dd60d5
yosu <woodstock830@gmail.com>
2015-05-21 14:03:40 +0900

    Update Brewfile and add vim

--- scripts/osx/install_brew.sh
+++ scripts/osx/install_brew.sh
@@ -4,6 +4,7 @@ if [[ ! -x /usr/local/bin/brew ]]; then
     echo 'installing brew ...'
     ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
     echo 'done'
+    brew tap Homebrew/bundle
 fi
 
 
